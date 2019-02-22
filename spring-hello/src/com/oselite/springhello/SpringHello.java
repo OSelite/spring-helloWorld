0eb997030097ebd4ad427664637b7b38cce13085
OSelite <oselite.dev@gmail.com>
2019-02-22 10:34:48 +0100

    Created SpringHello.java

--- spring-hello/src/com/oselite/springhello/SpringHello.java
+++ spring-hello/src/com/oselite/springhello/SpringHello.java
@@ -1,10 +1,21 @@
 package com.oselite.springhello;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SpringHello {
 
 	public static void main(String[] args) {
 
+		/** load the spring configuration file */
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
+		/** retrieve bean from spring container */
+		Animal myAnimal = context.getBean("cat", Animal.class);
+		//Animal myAnimal = context.getBean("dog", Animal.class);
+
+		System.out.println(myAnimal.voice());
+		
+		context.close();
 	}
 
 }
